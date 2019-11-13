11ca8114531b25e07cbccede7a8d09ef355ec4f0
Vincent Gouteux <vincent.gouteux8@gmail.com>
2019-11-13 18:34:41 +0100

    Plot accuracy and test

--- Projet DS 2 - Traduction.py
+++ Projet DS 2 - Traduction.py
@@ -2,7 +2,7 @@
 """
 Created on Mon Nov 11 13:31:38 2019
 
-@author: Maxen
+@author: Vince
 """
 
 
@@ -10,6 +10,8 @@ import io
 import numpy as np
 import sklearn
 from sklearn.metrics.pairwise import cosine_similarity
+import matplotlib.pyplot as plt
+
 
 #word embeddings : anglais et français
 
@@ -30,8 +32,8 @@ def load_vec(emb_path, nmax=50000):
     embeddings = np.vstack(vectors)
     return embeddings, id2word, word2id
 
-src_path = r'C:\Users\Maxen\Downloads\ok\ok\wiki.multi.fr.vec'
-tgt_path = r'C:\Users\Maxen\Downloads\ok\ok\wiki.multi.en.vec'
+tgt_path = '/Users/vince/DataProjetNLP/wiki.multi.en.vec.txt'
+src_path = '/Users/vince/DataProjetNLP/wiki.multi.fr.vec.txt'
 nmax = 50000  # maximum number of word embeddings to load
 
 src_embeddings, src_id2word, src_word2id = load_vec(src_path, nmax)
@@ -56,9 +58,8 @@ def load_dic(path):
     Z = np.vstack (vectors_tgt)
     return dico_full,X,Z
 
-path_train = r'C:\Users\Maxen\Downloads\ok\ok\fr-en.0-5000.txt'
-path_test = r'C:\Users\Maxen\Downloads\ok\ok\fr-en.5000-6500.txt'
-
+path_train = '/Users/vince/DataProjetNLP/fr-en.0-5000.txt'
+path_test = '/Users/vince/DataProjetNLP/fr-en.5000-6500.txt'
 dico_train, X_train, Z_train = load_dic(path_train)
 dico_test, X_test, Z_test = load_dic(path_test)
 
@@ -77,38 +78,41 @@ def dC_dW(W,X,Z):
     return S
 
 #W = np.random.rand(300,300)
-W = np.eye(300)
-eta = 0.001
-delta = 0.01
-N = 300
-nb = 1000
-
-#reprendre code theo 
-
-#descente de gradient  
-#def gradientDescent(eta,):
-valeur_C = []
-for t in range(N):
-    print(t)
-    tmp_W = W 
-    W = tmp_W - eta*dC_dW(tmp_W,X_train,Z_train)
-    valeur_C.append(C(W,X_train,Z_train))
-print(valeur_C)
-print(dC_dW(W,X_train,Z_train))
-print(np.linalg.norm(dC_dW(W,X_train,Z_train)))
+#eta = 0.001
+ #N = 300
+def gradientDescent(eta,N):
+    #W = np.eye(300)
+    W = np.random.rand(300,300)
+    valeur_C = []
+    acc_train = []
+    acc_test = []
+    for t in range(N):
+        if (t%10==0):
+            print(t)
+            dico_pred_test = prediction_dict(dico_test,W)  
+            acc_test.append(accuracy(dico_pred_test,dico_test))
+            #dico_pred_train = prediction_dict(dico_train,W)  
+            #acc_train.append(accuracy(dico_pred_train,dico_train))
+        tmp_W = W 
+        W = tmp_W - eta*dC_dW(tmp_W,X_train,Z_train)
+        valeur_C.append(C(W,X_train,Z_train))
+    #print(valeur_C)
+    #print(dC_dW(W,X_train,Z_train))
+    #print(np.linalg.norm(dC_dW(W,X_train,Z_train)))
+    return (W,valeur_C,acc_test,acc_train)
 
 #ou descente de gradient stochastique
-
-norm_grad = []
-for t in range(N):
-    print(t)
-    l = np.random.randint(low=0,high=len(dico_train)) 
-    tmp_W = W 
-    W = W - eta*dC_dW(tmp_W,X_train,Z_train)
-    valeur_C.append(C(W,X_train,Z_train))
-print(valeur_C)
-print(dC_dW(W,X_train,Z_train))
-print(np.linalg.norm(dC_dW(W,X_train,Z_train)))
+def SGD()
+    norm_grad = []
+    for t in range(N):
+        print(t)
+        l = np.random.randint(low=0,high=len(dico_train)) 
+        tmp_W = W 
+        W = W - eta*dC_dW(tmp_W,X_train,Z_train)
+        valeur_C.append(C(W,X_train,Z_train))
+    print(valeur_C)
+    print(dC_dW(W,X_train,Z_train))
+    print(np.linalg.norm(dC_dW(W,X_train,Z_train)))
 
 
 #ORTHOGONAL TRANSFORM 
@@ -122,18 +126,38 @@ def prediction(W,mot):
     z_pred = np.argmax(sklearn.metrics.pairwise.cosine_similarity(z.reshape(1,300),tgt_embeddings)) #celui qui a la plus grande similarite cos avec z
     return tgt_id2word[z_pred]
 
-dico_pred={}
-i=0
-for mot in dico_test.keys() :
-    print(i)
-    dico_pred[mot]=prediction(W,mot)
-    i+=1
 
-def accuracy(dico_pred,dico_test):
+def prediction_dict(dico,W):
+    dico_pred={}
+    i=0
+    for mot in dico.keys() :
+        if (i%10==0):
+            print(i)
+        dico_pred[mot]=prediction(W,mot)
+        i+=1
+    return dico_pred 
+
+def accuracy(dico_pred,dico):
     c=0
-    for key in dico_test.keys():
-        if dico_test[key] == dico_pred[key]:
+    for key in dico.keys():
+        if dico[key] == dico_pred[key]:
             c+=1
-    return(c/len(dico_test)) #nb de mots bien prédits/nb de mots total
+    return(c/len(dico)) #nb de mots bien prédits/nb de mots total
     
+W, val, aac_test, acc_train = gradientDescent(0.001,30)
+plt.title("Fonction de cout en fonction des itérations")
+plt.plot(range(len(val)),val)
+plt.ylabel('CostFunction')
+plt.xlabel('Iteration')
+plt.legend()
+plt.show()
+dico_pred = prediction_dict(dico_test)  
 accuracy_test = accuracy(dico_pred,dico_test)
+
+plt.plot(range(0,len(val),10),aac_test)
+plt.ylabel('Accuracy TestSet')
+plt.xlabel('Iteration')
+plt.legend()
+plt.show()
+def plot_accuracy():
+    
